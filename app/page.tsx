7dfd1cbc9f4eb120564f5ef85e85b16e21983806
Amaury <amauryfranssen@gmail.com>
2026-03-23 14:44:32 +0100

    feat(ui): create a form and callthe location route to locate the user input

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import { Section } from "@components/sections/Section";
-import { MapContainer } from "@components/containers/MapContainer";
-import { HeroBanner } from "@components/molecules/HeroBanner";
-import { AddressInfosContainer } from "@components/containers/AddressInfosContainer";
-import { BannerSection } from "@components/sections/BannerSection";
+import { HomeInteractive } from "@components/containers/HomeInteractive";
 import { Header } from "@modules/app/react/layout/Header";
 import { presentHomeViewModel } from "@presenters/presentHome.server";
 
@@ -13,21 +9,7 @@ export default async function Home() {
     <>
     <Header />
     <main className="flex min-h-[calc(100vh-var(--header-height)-var(--banner-height))] w-full flex-col">
-      <BannerSection>
-        <HeroBanner hasIpifyCredits={vm.hasIpifyCredits} />
-        <AddressInfosContainer
-          ipAddress={vm.ipAddress}
-          locationCity={vm.locationCity}
-          locationTimezone={vm.locationTimezone}
-          isp={vm.isp}
-        />
-      </BannerSection>
-      <Section
-        fluid
-        classNames="flex-1 min-h-[300px] min-w-[300px] flex justify-center items-center"
-      >
-        <MapContainer coordinates={vm.coordinates} />
-      </Section>
+      <HomeInteractive initialViewModel={vm} />
     </main>
     </>
   );
